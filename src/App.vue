5a9a0b3bd239e0c6c8981f666a733e2f551784f0
RiceMTB <137529117+RiceMTB@users.noreply.github.com>
2023-06-23 15:09:20 -0500

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <myheader></myheader>
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
+    <img alt="Vue logo" src="https://us-east-1-02890046-view.menlosecurity.com/c/0/i/aHR0cHM6Ly93d3cuZGlzcGF0Y2guY29tL2djZG4vcHJlc3RvLzIwMjMvMDIvMjgvTkNPRC8wYmIxNjU3Ni1lNTI5LTQyYjAtOWZhZi01NTcxMGJiOWJjNmQtMjcyMjA5XzE1MzZfcmdiXzEuanBnP3dpZHRoPTk4MCZoZWlnaHQ9Njk2JmZpdD1jcm9wJmZvcm1hdD1wanBnJmF1dG89d2VicA~~">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
      <myfooter></myfooter>
   </div>
