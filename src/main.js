59e72d8cbaf0b8189edf85e00ac6ab56fcf9931d
RiceMTB <137529117+RiceMTB@users.noreply.github.com>
2023-06-28 13:26:49 -0500

    1 file modified

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ const { createApp } = require('vue');
 import App from "./App.vue";
 import createRouter from './router';
 
-createApp(App).use(createRouter())
-createApp(App).mount("#app");
+createApp(App) 
+  .use(createRouter())
+  .mount("#app");
