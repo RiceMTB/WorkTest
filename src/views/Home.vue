ab74e2796c7def037adc75399e146fc9caf1ac4e
RiceMTB <137529117+RiceMTB@users.noreply.github.com>
2023-06-28 13:13:40 -0500

    4 files added

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <btn v-on:click="testClick()">Test</btn>
+    <btn href="https://www.google.com/">  Test2</btn> 
+    <btn>Test3</btn>
+
+  </div>
+</template>
+
+<script>
+import btn from  '/components/ourBtn.vue'
+export default {
+  name: 'HelloWorld',
+  props: {
+   
+  },
+  components: {
+    btn
+  },
+  methods:{ 
+    testClick:function(){ 
+      alert("btn TEST clicked")
+    }
+  }
+}
+</script>
